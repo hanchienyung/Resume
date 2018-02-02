9e66043b2ccabf3181545377940bc5900bf0bbb0
hanchienyung <hanchienyung@yahoo.com>
2018-02-02 01:57:35 -0500

    Initial Commit

--- src/main/java/com/cy/example/demo/ResumeController.java
+++ src/main/java/com/cy/example/demo/ResumeController.java
@@ -2,7 +2,14 @@ package com.cy.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class ResumeController {
@@ -11,12 +18,28 @@ public class ResumeController {
     ResumeRepository resumeRepository;
 
 
-
     @RequestMapping("/")
-    public String listEducations();
+    public String listResume(Model model){
+        model.addAttribute("resumes", resumeRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String resumeForm(Model model){
+        model.addAttribute("resume", new Resume());
+        return "resumeform";
+    }
 
 
-    @RequestMapping("/")
-    public String listExperiences();
+    @PostMapping("/process")
+    public String processForm(@Valid Resume resume, BindingResult result)
+    {
+        if (result.hasErrors()){
+            return "resumeform";
+        }
+        resumeRepository.save(resume);
+
+        return "redirect:/";
+    }
 
 }
